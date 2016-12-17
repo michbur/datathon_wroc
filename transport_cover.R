f10275d57b1f3d333d41bc3c1d9f5f9e7ad73fdc
michbur <michalburdukiewicz@gmail.com>
2016-12-17 17:39:15 +0100

    added points covered

--- transport_cover.R
+++ transport_cover.R
@@ -1,24 +1,38 @@
 library(dplyr)
 
 cov_dat <- read.csv("data/BAZA-MPK.csv")
+stops <- read.table("data/stops.txt", sep = ",", header = TRUE)
 
-select(cov_dat, Kierunek) %>% 
-  filter(!duplicated(.)) %>% 
-  mutate(Kierunek = as.character(Kierunek)) %>% 
-  arrange(Kierunek) %>% 
-  mutate(kierunek_lc = tolower(Kierunek)) %>% 
-  write.csv("direction_names.csv")
+select(cov_dat, Kierunek) %>%
+  filter(!duplicated(.)) %>%
+  mutate(Kierunek = as.character(Kierunek)) %>%
+  arrange(Kierunek) %>%
+  mutate(kierunek_lc = tolower(Kierunek)) %>%
+  write.csv2("./results/direction_names.csv", row.names = FALSE)
 
-select(cov_dat, Nazwa.przystanku, linia, Kierunek) %>% 
-  filter(!duplicated(.)) %>% 
-  arrange(linia)
+# repletion - hour data
+reph_dat <- select(cov_dat, Nazwa.przystanku, Kierunek, Godzinarzeczywista, Napełnienie) %>% 
+  mutate(hour = substr(Godzinarzeczywista, 0, 2)) %>% 
+  select(-Godzinarzeczywista) %>% 
+  group_by(Nazwa.przystanku, Kierunek, hour) %>% 
+  summarise(nap_sum = mean(Napełnienie)) %>% 
+  ungroup
 
-filter(cov_dat, Nazwa.przystanku == "RYNEK (KAZIMIERZA WIELKIEGO/KRUPNICZA)")
 
-unique(cov_dat[["Nazwa.przystanku"]])
-lapply(as.character(unique(cov_dat[["Kierunek"]])), function(i) {
-  both_parts <- strsplit(i, "(", fixed = TRUE)[[1]]
-  part1 <- both_parts[1]
-  part2 <- strsplit(both_parts[2], ")", fixed = TRUE)[[1]][1]
-  c(name = part1, id = part2)
-})
+library(ggplot2)
+filter(reph_dat, Nazwa.przystanku == "PLAC BEMA") %>% 
+  ggplot(aes(x = hour, y = nap_sum)) +
+  geom_bar(stat = "identity") +
+  facet_wrap(~ Kierunek)
+
+select(cov_dat, linia, Nazwa.przystanku) %>% 
+  filter(!duplicated(.)) 
+
+lapply(as.character((cov_dat[["Kierunek"]])), function(i) {
+  splitted <- strsplit(i, "\\([0-9]")[[1]]
+  c(name = splitted[1], id = strsplit(splitted[2], ")")[[1]][1])
+  }) %>% 
+  do.call(rbind, .) %>% 
+  edit
+
+cov_dat[["Nazwa.przystanku"]]
